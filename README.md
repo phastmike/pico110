aeb3c34afcb7d9bd059cc5b0cc9bce494c9001a8
José Miguel Fonte <phastmike@gmail.com>
2021-11-03 02:06:07 +0000

    clean the code a bit, links in readme

--- README.md
+++ README.md
@@ -15,6 +15,21 @@
 - Power (P/PA/PO) Hi/Lo (Watts)
 - TxAdmit (Tad = MO/AL/NC/NC.PL)
 
+- Display Brightness
+- Screen on/off
+
 ### Missing
 - Memories (Name and Frequency)
 - How to memorize, Edit, Remove Memories (CRUD)
+- Dual VFO with same/distinct steps etc.
+
+### New control head
+- Need a way to open Squelch
+
+## Links
+
+- https://github.com/thilaire/rpi-TM1638
+- https://github.com/wahlencraft/TM1637-pico
+- https://github.com/lpodkalicki/attiny-tm1638-library
+- https://github.com/gavinlyonsrepo/TM1638plus
+- https://github.com/rjbatista/tm1638-library
--- test.c
+++ test.c
@@ -32,85 +32,87 @@
 #include "m110.h"
 #include "tm1638.h"
 #include "tune_step.h"
-#include "pico/stdlib.h"
+#include "rom_init.h"
+#include "i2c0.h"
+#include <pico/stdlib.h>
 //#include "hardware/i2c.h"
 //#include "hardware/irq.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "rom_init.h"
-#include "i2c0.h"
-
-// define I2C addresses to be used for this peripheral
-//#define I2C0_PERIPHERAL_ADDR 0x50
-
-// GPIO pins to use for I2C
-//#define GPIO_SDA0 12
-//#define GPIO_SCK0 13
 
 // ADDED LED PIN
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 // INIT EEPROM WITH CONTENTS
-m110_t      *m110;
+m110_t        *m110;
+double         freq = 446.00625;
+tune_step_t   *tune_step = NULL; 
 
-double      freq = 446.00625;
-tune_step_t *tune_step = NULL; 
 
-/*
-void i2c0_irq_handler() {
-
-   // Get interrupt status
-   uint32_t status = i2c0->hw->intr_stat;
-
-   // Check to see if we have received data from the I2C controller
-   if (status & I2C_IC_INTR_STAT_R_RX_FULL_BITS) {
-
-      // Read the data (this will clear the interrupt)
-      uint32_t value = i2c0->hw->data_cmd;
-
-      // Check if this is the 1st byte we have received
-      if (value & I2C_IC_DATA_CMD_FIRST_DATA_BYTE_BITS) {
-
-         // If so treat it as the address to use
-         eeprom_addr_set(m110_eeprom_get(m110), (uint16_t)(value & I2C_IC_DATA_CMD_DAT_BITS));
-         //eeprom_addr_set((eeprom_t *) m110, (uint16_t)(value & I2C_IC_DATA_CMD_DAT_BITS));
-      } else {
-         // If not 1st byte then store the data in the RAM
-         // and increment the address to point to next byte
-         eeprom_write_byte(m110_eeprom_get(m110), (uint8_t)(value & I2C_IC_DATA_CMD_DAT_BITS));
-         //eeprom_write_byte((eeprom_t *) m110, (uint8_t)(value & I2C_IC_DATA_CMD_DAT_BITS));
-      }
-   }
-
-   // Check to see if the I2C controller is requesting data from the RAM
-   if (status & I2C_IC_INTR_STAT_R_RD_REQ_BITS) {
-
-      // Write the data from the current address in RAM
-      i2c0->hw->data_cmd = (uint32_t) eeprom_read_byte(m110_eeprom_get(m110));
-      //i2c0->hw->data_cmd = (uint32_t) eeprom_read_byte((eeprom_t *) m110);
-
-      // Clear the interrupt
-      i2c0->hw->clr_rd_req;
-   }
-}
-*/
-
+/* Convert a frequency into a string.
+ * Returns a newly allocated string,
+ *  caller must free the string
+ */
 unsigned char *freq2string(double freq) {
    unsigned char i, j;
-   unsigned char *ft, *string;
-   ft = (unsigned char *) calloc (1, 9);
-   sprintf(ft, "%f", freq);
-   string = (unsigned char *) calloc (1, 11);
+   unsigned char *fstr, *rstr;
+   fstr = (unsigned char *) calloc (1, 9);
+   sprintf(fstr, "%f", freq);
+   rstr = (unsigned char *) calloc (1, 11);
    for (i = 0, j = 0; i < 9; i++, j++) {
       if (i == 7) {
-         string[j] = '.';
+         rstr[j] = '.';
          j++;
       }
-      string[j] = ft[i];
+      rstr[j] = fstr[i];
    }
+   free(fstr);
+   return rstr;
+}
 
-   return string;
+void radio_set_frequency(m110_t *m110, double freq_rx, double freq_tx) {
+   m110_channel_frequencies_set(m110, 1, freq_rx, freq_tx);
+   m110_channel_frequencies_set(m110, 2, freq_rx, freq_tx);
+}
+
+void display_show_frequency(tm1638_t *tm1638, double freq) {
+   unsigned char *s = freq2string(freq);
+   tm1638_show(tm1638, s, 0);
+   free(s);
+}
+
+void display_show_eeprom_n_and_a(tm1638_t *tm1638, m110_t *m110) {
+   //tm1638_clear(tm1638);
+   unsigned char b1 = eeprom_raw_read_byte(m110_eeprom_get(m110), 0x22);
+   unsigned char b2 = eeprom_raw_read_byte(m110_eeprom_get(m110), 0x23);
+   unsigned char b3 = eeprom_raw_read_byte(m110_eeprom_get(m110), 0x24);
+   unsigned int N = ((b1 & 2) << 8) + b2;
+   unsigned int A = b3 >> 1;
+   unsigned char *str = calloc(1, 8);
+   sprintf(str, "N.%3dA.%3d",N,A);
+   printf("str=[%s]\n", str);
+   tm1638_show(tm1638, str, 0);
+   free(str);
+}
+
+void display_show_tune_step(tm1638_t *tm1638, tune_step_t *tune_step) {
+   unsigned char *str = calloc(1, 8);
+   unsigned char *step_str = tune_step_get_as_string(tune_step);
+   sprintf(str, "TS  %s",step_str);
+   tm1638_show(tm1638, str, 0);
+   free(str);
+   free(step_str);
+}
+
+void display_show_brightness(tm1638_t *tm1638) {
+   unsigned char b = tm1638_brightness_get(tm1638);
+   if (b >= 0 && b <= 7) {
+      unsigned char *str = calloc(1, 8);
+      sprintf(str, "Bright %d", b);
+      tm1638_show(tm1638, str, 0);
+      free(str);
+   }
 }
 
 void on_step_changed(tune_step_t *tune_step, void *user_data) {
@@ -119,10 +121,13 @@ void on_step_changed(tune_step_t *tune_step, void *user_data) {
 
 // Main loop - initilises system and then loops while interrupts get on with processing the data
 int main() {
+   unsigned int keys;
+   unsigned char opt_function_state = false;
+
    m110 = m110_new_with_data(rom_init);
 
    tune_step = tune_step_new();
-   tune_step_on_changed_connect(tune_step, on_step_changed, "test userdata");
+   tune_step_on_changed_connect(tune_step, on_step_changed, "[This is userdata]");
 
    stdio_init_all();
 
@@ -130,46 +135,15 @@ int main() {
    gpio_set_dir(LED_PIN, GPIO_OUT);
 
    i2c0_init(0);
-   /*
-   // Setup I2C0 as slave (peripheral)
-   i2c_init(i2c0, 100 * 1000);
-   i2c_set_slave_mode(i2c0, true, I2C0_PERIPHERAL_ADDR);
 
-   // Enable slave clock stretching
-   //i2c0->hw->enable = 0;
-   //hw_set_bits(&i2c0->hw->con, I2C_IC_CON_RX_FIFO_FULL_HLD_CTRL_BITS);
-   //i2c0->hw->enable = 1;
-   //-----------------------------------------------------------------
-
-   // Setup GPIO pins to use and add pull up resistors
-   gpio_set_function(GPIO_SDA0, GPIO_FUNC_I2C);
-   gpio_set_function(GPIO_SCK0, GPIO_FUNC_I2C);
-   gpio_pull_up(GPIO_SDA0);
-   gpio_pull_up(GPIO_SCK0);
-
-   // Enable the I2C interrupts we want to process
-   i2c0->hw->intr_mask = (I2C_IC_INTR_MASK_M_RD_REQ_BITS | I2C_IC_INTR_MASK_M_RX_FULL_BITS);
-
-   // Set up the interrupt handler to service I2C interrupts
-   //irq_set_exclusive_handler(I2C0_IRQ, i2c0_irq_handler0);
-   irq_set_exclusive_handler(I2C0_IRQ, i2c0_irq_handler);
-
-   // Enable I2C interrupt
-   irq_set_enabled(I2C0_IRQ, true);
-   */
-
-   // Do nothing in main loop
    tm1638_t *tm1638;
    tm1638 = tm1638_new(1, 2, 3, 0);
-   unsigned int keys;
 
    unsigned char *si = freq2string(freq);
    tm1638_show(tm1638, si, 0);
    free(si);
-   m110_channel_frequencies_set(m110, 1, freq, freq);
-   m110_channel_frequencies_set(m110, 2, freq, freq);
 
-   unsigned char opt_function_state = false;
+   radio_set_frequency(m110, freq, freq);
 
    while (true) {
       tight_loop_contents();
@@ -186,69 +160,43 @@ int main() {
          tm1638_led(tm1638, 0, opt_function_state);
 
          if (!opt_function_state) {
-            unsigned char *si = freq2string(freq);
-            tm1638_show(tm1638, si, 0);
-            free(si);
-            m110_channel_frequencies_set(m110, 1, freq, freq);
-            m110_channel_frequencies_set(m110, 2, freq, freq);
+            display_show_frequency (tm1638, freq);
+            radio_set_frequency(m110, freq, freq);
          } else {
-            unsigned char *str = calloc(1, 8);
-            unsigned char *step_str = tune_step_get_as_string(tune_step);
-            sprintf(str, "TS  %s", step_str);
-            tm1638_show(tm1638, str, 0);
-            free(str);
-            free(step_str);
+            display_show_tune_step(tm1638, tune_step);
          }
       } else if (keys & 2) {
          if (opt_function_state) {
             tm1638_led(tm1638, tune_step_get_index(tune_step) + 1, 0); 
             tune_step_next(tune_step);
             tm1638_led(tm1638, tune_step_get_index(tune_step) + 1, 0); 
-            unsigned char *str = calloc(1, 8);
-            unsigned char *step_str = tune_step_get_as_string(tune_step);
-            sprintf(str, "TS  %s",step_str);
-            tm1638_show(tm1638, str, 0);
-            free(str);
-            free(step_str);
+            display_show_tune_step(tm1638, tune_step);
          } else {
-            tm1638_clear(tm1638);
+            display_show_eeprom_n_and_a(tm1638, m110);
          }
       } else if (keys & 4) {
          tm1638_power_set(tm1638, !tm1638_power_get(tm1638));
          sleep_ms(50);
       } else if (keys & 8) {
-         tm1638_show(tm1638, "Press F", 0);
+         tm1638_show(tm1638, "Press  F", 0);
       } else if (keys & 16) {
          unsigned char b = tm1638_brightness_get(tm1638);
          b -= 1;
          tm1638_brightness_set(tm1638, b);
-         b = tm1638_brightness_get(tm1638);
-         unsigned char *str = calloc(1, 8);
-         sprintf(str, "Bright %d", b);
-         tm1638_show(tm1638, str, 0);
+         display_show_brightness(tm1638);
       } else if (keys & 32) {
          unsigned char b = tm1638_brightness_get(tm1638);
          b += 1;
          tm1638_brightness_set(tm1638, b);
-         b = tm1638_brightness_get(tm1638);
-         unsigned char *str = calloc(1, 8);
-         sprintf(str, "Bright %d", b);
-         tm1638_show(tm1638, str, 0);
-         free(str);
+         display_show_brightness(tm1638);
       } else if (keys & 64) {
          freq -= tune_step_get_as_MHz(tune_step);
-         unsigned char *s = freq2string(freq);
-         tm1638_show(tm1638, s, 0);
-         free(s);
-         m110_channel_frequencies_set(m110, 1, freq, freq);
-         m110_channel_frequencies_set(m110, 2, freq, freq);
+         display_show_frequency (tm1638,freq);
+         radio_set_frequency(m110, freq, freq);
       } else if (keys & 128) {
          freq += tune_step_get_as_MHz(tune_step);
-         unsigned char *s = freq2string(freq);
-         tm1638_show(tm1638, s, 0);
-         free(s);
-         m110_channel_frequencies_set(m110, 1, freq, freq);
-         m110_channel_frequencies_set(m110, 2, freq, freq);
+         display_show_frequency (tm1638,freq);
+         radio_set_frequency(m110, freq, freq);
       } 
       gpio_put(LED_PIN, 0);
       sleep_ms(50);
