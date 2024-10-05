2a6e84b6a7c9b48f77d04206e65591e628fc4a0d
Jose Miguel Fonte <phastmike@gmail.com>
2024-10-05 20:02:27 +0100

    bumped to 0.2.4

--- src/vc_version.c
+++ src/vc_version.c
@@ -33,7 +33,7 @@ vc_version_t *vc_version_new(hmi_t *hmi, radio_t *radio) {
 void vc_version_show(view_controller_t *vc) {
    assert(vc != NULL);
    char *string = (char *) calloc (1,9);
-   sprintf(string, "Rel  0.2.3");
+   sprintf(string, "Rel  0.2.4");
    hmi_display_text(vc->hmi, 0, string);
    free(string);
 }
